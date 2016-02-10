366f6648e52c1b34056061d125799fc8bcc11d0c
terehov <terehovsergej@gmail.com>
2016-02-10 02:22:26 +0200

    in process

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,42 @@
+jQuery.fn.exists = function() {
+    return $(this).length;
+}
+
+
 var app = app || {};
 app.main = {
     init: function() {
-        this.events();
+        this.tabs();
+        this.carouselInit();
     },
-    events: function () {
-        console.log('test');
+    tabs: function () {
+        var $tabsBlock = $('.tabs-block');
+        if ($tabsBlock.length > 0) {
+            $('.jsTabLink').on('click', function (e) {
+                e.preventDefault();
+                var $this = $(this),
+                    tab = $this.attr('href'),
+                    $tab = $(tab);
+                if (!$this.hasClass('active')) {
+                    $('.jsTabLink').removeClass('active');
+                    $this.addClass('active');
+                    $('.tabs__content').hide().removeClass('active');
+                    $tab.fadeIn(250).addClass('active');
+                }
+
+            })
+        }
+    },
+    carouselInit: function () {
+        var $carousel = $('.owl-carousel');
+        if ($carousel.exists()) {
+            $carousel.owlCarousel({
+                items: 3,
+                navigation: true,
+                navigationText: false,
+                pagination: false
+            });
+        }
     }
 };
 var App = (function($, app){
