a2a05e8b7012fd256095593fc50b1b79ccb441d2
terehov <terehovsergej@gmail.com>
2016-03-06 13:57:29 +0200

    in process

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 jQuery.fn.exists = function() {
     return $(this).length;
-}
+};
 
 
 var app = app || {};
@@ -8,6 +8,7 @@ app.main = {
     init: function() {
         this.tabs();
         this.carouselInit();
+        this.catalogAccordion();
     },
     tabs: function () {
         var $tabsBlock = $('.tabs-block');
@@ -36,6 +37,27 @@ app.main = {
                 navigationText: false,
                 pagination: false
             });
+        }
+    },
+    catalogAccordion: function () {
+        var $accordion = $('.catalog-accordion');
+        if ($accordion.exists()) {
+            var $accToggle = $accordion.find('.accordion-toggle'),
+                $accInner = $accordion.find('.accordion-inner');
+            $accToggle.on('click', function (e) {
+                e.preventDefault();
+                var $this = $(this),
+                    $thisInner = $this.next('.accordion-inner');
+                if (!$this.hasClass('active')) {
+                    $accToggle.removeClass('active');
+                    $accInner.slideUp(function () {
+                        $(this).removeClass('active');
+                    });
+                    $this.addClass('active');
+                    $thisInner.slideDown().addClass('active');
+                }
+            })
+
         }
     }
 };
